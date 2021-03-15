96ad5aa80dd010dc5649c05641c5e2bfd7dfb888
Srinivasan Sekar <srinivasan.sekar1990@gmail.com>
2021-03-15 17:21:22 +0530

    fetch android device from appium adb

--- src/plugin.js
+++ src/plugin.js
@@ -1,3 +1,15 @@
 import BasePlugin from '@appium/base-plugin';
+import ADB from 'appium-adb';
 
-export default class DevicePlugin extends BasePlugin {}
+export default class DevicePlugin extends BasePlugin {
+  async createSession(next, driver, jwpDesCaps, jwpReqCaps, caps) {
+    const adb = await ADB.createADB();
+    const connectedDevices = await adb.getConnectedDevices();
+    if (connectedDevices.length > 0) {
+      caps.firstMatch[0]['appium:deviceName'] = connectedDevices[0].udid;
+    }
+    return await driver.createSession(jwpDesCaps, jwpReqCaps, caps);
+    // const response = await next();
+    // return response;
+  }
+}
