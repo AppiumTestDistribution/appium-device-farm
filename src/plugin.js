a1f2c205f09e13904365519ce9f89bc9971bfe70
saikrishna321 <saikrishna321@yahoo.com>
2021-03-16 07:56:41 +0530

    Assign free systemPort capability

--- src/plugin.js
+++ src/plugin.js
@@ -1,15 +1,29 @@
 import BasePlugin from '@appium/base-plugin';
 import ADB from 'appium-adb';
+let portfinder = require('portfinder');
 
+let connectedDevices;
+let freePort;
+portfinder.basePort = 60535;
 export default class DevicePlugin extends BasePlugin {
+  constructor(pluginName) {
+    super(pluginName);
+  }
   async createSession(next, driver, jwpDesCaps, jwpReqCaps, caps) {
+    await this.getFreePort();
+    console.log('FreePort', freePort);
     const adb = await ADB.createADB();
-    const connectedDevices = await adb.getConnectedDevices();
+    connectedDevices = await adb.getConnectedDevices();
     if (connectedDevices.length > 0) {
       caps.firstMatch[0]['appium:deviceName'] = connectedDevices[0].udid;
+      caps.firstMatch[0]['appium:systemPort'] = freePort;
     }
     return await driver.createSession(jwpDesCaps, jwpReqCaps, caps);
-    // const response = await next();
-    // return response;
+  }
+
+  async getFreePort() {
+    await portfinder.getPort(function (err, port) {
+      freePort = port;
+    });
   }
 }
