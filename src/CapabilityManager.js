8085059d687713dd510fbf7a96b01270265a5f2d
saikrishna321 <saikrishna321@yahoo.com>
2021-04-02 07:35:46 +0530

    added chromeDriverPort for parallel webview session on android

--- src/CapabilityManager.js
+++ src/CapabilityManager.js
@@ -4,6 +4,7 @@ export async function androidCapabilities(caps, freeDevice) {
   caps.firstMatch[0]['appium:udid'] = freeDevice.udid;
   caps.firstMatch[0]['appium:deviceName'] = freeDevice.udid;
   caps.firstMatch[0]['appium:systemPort'] = await getPort();
+  caps.firstMatch[0]['appium:chromeDriverPort'] = await getPort();
 }
 
 export async function iOSCapabilities(caps, freeDevice) {
