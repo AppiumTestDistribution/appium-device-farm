093140905a1d1a47d73ee038db3c80828c8552c7
Sai Krishna <saikrishna321@yahoo.com>
2021-04-14 23:23:20 +0530

    Assign Device based on platform (#26)
    * refactor
    
    * DI - To write unit test
    
    * remove unused dependencies

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,11 @@
+import os from 'os';
+
 export async function asyncForEach(array, callback) {
   for (let index = 0; index < array.length; index++) {
     await callback(array[index], index, array);
   }
+}
+
+export function isMac() {
+  return os.type() === 'Darwin';
 }
