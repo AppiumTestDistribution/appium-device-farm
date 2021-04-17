c6e12b726ad67139d72d23ec7f3bfded841d49ed
saikrishna321 <saikrishna321@yahoo.com>
2021-04-17 16:00:10 +0530

    added function to check if config path is absolute

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,5 @@
 import os from 'os';
+import path from 'path';
 
 export async function asyncForEach(array, callback) {
   for (let index = 0; index < array.length; index++) {
@@ -8,4 +9,8 @@ export async function asyncForEach(array, callback) {
 
 export function isMac() {
   return os.type() === 'Darwin';
+}
+
+export function checkIfPathIsAbsolute(configPath) {
+  return path.isAbsolute(configPath);
 }
---
+++ test/config.spec.js
@@ -0,0 +1,20 @@
+import path from 'path';
+import { isDeviceConfigPathAbsolute } from '../src/Devices';
+import { expect } from 'chai';
+
+describe.only('Config Test', () => {
+  it('Should be able to load data from absolute path from config', async () => {
+    const deviceConfigPath = path.resolve(
+      __dirname,
+      './fixtures/device.config.js'
+    );
+    expect(isDeviceConfigPathAbsolute(deviceConfigPath)).to.be.true;
+  });
+
+  it('Should throw error when path is not absolute', async () => {
+    const deviceConfigPath = './fixtures/device.config.js';
+    expect(() => isDeviceConfigPathAbsolute(deviceConfigPath)).to.throw(
+      'Device Config Path ./fixtures/device.config.js should be absolute'
+    );
+  });
+});
