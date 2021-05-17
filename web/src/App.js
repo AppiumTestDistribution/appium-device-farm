c0088f2ad4bb6ac0b6588a56633a41b03a9c59eb
Srinivasan Sekar <srinivasan.sekar1990@gmail.com>
2021-05-17 17:25:02 +0530

    Appium Device Plugin is Appium Device Farm now

--- web/src/App.js
+++ web/src/App.js
@@ -4,7 +4,7 @@ import DevicesContainer from './DevicesContainer';
 const App = () => (
   <div className="App">
     <div className="header">
-      <h1 className="mb-4 mt-4 ml-2">Appium Device Plugin</h1>
+      <h1 className="mb-4 mt-4 ml-2">Appium Device Farm</h1>
     </div>
     <DevicesContainer />
   </div>
