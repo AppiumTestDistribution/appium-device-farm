fcd29188a46ff9681fcc9e4a89cb311acef0c52a
Srinivasan Sekar <srinivasan.sekar1990@gmail.com>
2021-07-02 18:41:50 +0530

    UI fixes

--- web/src/PendingSessionsCount.js
+++ web/src/PendingSessionsCount.js
@@ -25,18 +25,14 @@ const PendingSessionsCount = ({children}) => {
     },[] );
     if (loading) {
         return (
-            <div className="d-flex flex-column bd-highlight mt-4 text-center">
-                <div className="p-2 bd-highlight">Loading pending tests</div>
-            </div>
+            <div>Loading pending tests</div>
         );
     } else if (error) {
         return (
-            <div className="d-flex flex-column bd-highlight mb-4 text-center">
-                <div className="p-2 bd-highlight">Something went wrong</div>
-            </div>
+            <div>Something went wrong</div>
         );
     } else {
-        return <div className="pending-tests mt-4 p-2 bd-highlight">{children}Queued Tests : {pendingTests}  </div>;
+        return <div className="pending-tests">{children}Queued Tests : {pendingTests}  </div>;
     }
 };
 export default PendingSessionsCount;
