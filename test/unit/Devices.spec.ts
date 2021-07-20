3063508e075bcb9f91239543871dbfff0b6e8a5f
Manjot Kaur <manjotkaur@Manjots-MacBook-Pro.local>
2021-07-20 16:02:14 +0530

    [Palak|Areeba|Manjot] Refactor. Remove unused imports in  device test

--- test/unit/Devices.spec.ts
+++ test/unit/Devices.spec.ts
@@ -1,9 +1,7 @@
-// import { expect } from 'chai';
 import Devices, {
   listAllDevices,
   findUserSpecifiesDevices,
 } from '../../src/Devices';
-// import { deviceMock } from './fixtures/devices';
 
 describe('Devices', () => {
   const deviceMock = [
