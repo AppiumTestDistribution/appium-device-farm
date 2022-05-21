5d9a0a44090f21d65a5c6014efb45d74716122a0
saikrishna321 <saikrishna321@yahoo.com>
2022-05-21 08:28:14 +0530

    fix appium support import

--- src/logger.ts
+++ src/logger.ts
@@ -1,3 +1,3 @@
-import { logger } from 'appium-support';
+import { logger } from '@appium/support';
 const log = logger.getLogger('device-farm');
 export default log;
--- typings/appium-support/index.d.ts
+++ typings/appium-support/index.d.ts
@@ -1,1 +1,1 @@
-declare module 'appium-support';
+declare module '@appium/support';
