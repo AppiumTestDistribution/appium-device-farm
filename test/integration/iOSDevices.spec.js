8968bdfa65fe81bee0b2fd4f8effab34f10636ad
Joel Whitney <joel.whitney@kandji.io>
2022-07-12 10:24:55 -0400

    fix ios integration tests

--- test/integration/iOSDevices.spec.js
+++ test/integration/iOSDevices.spec.js
@@ -8,7 +8,6 @@ describe('IOS Test', () => {
   it('Throw error when no device is found for given capabilities', async () => {
     const deviceManager = new DeviceFarmManager({
       platform: 'iOS',
-      includeSimulators: true,
     });
     Container.set(DeviceFarmManager, deviceManager);
     await updateDeviceList();
@@ -30,51 +29,5 @@ describe('IOS Test', () => {
           'No device found for filters: {"platform":"ios","name":"iPhone","deviceType":"real","busy":false,"offline":false}'
         )
     );
-  });
-
-  it('Should find free iPhone simulator when app path has .app extension and set busy status to true', async () => {
-    const deviceManager = new DeviceFarmManager({
-      platform: 'ios',
-      includeSimulators: true,
-    });
-    Container.set(DeviceFarmManager, deviceManager);
-    await updateDeviceList();
-    const capabilities = {
-      alwaysMatch: {
-        platformName: 'iOS',
-        'appium:app': '/Downloads/VodQA.app',
-        'appium:iPhoneOnly': true,
-        'appium:deviceAvailabilityTimeout': 1800,
-        'appium:deviceRetryInterval': 100,
-      },
-      firstMatch: [{}],
-    };
-    const device = await DevicePlugin.allocateDeviceForSession(capabilities);
-    const allocatedSimulator = DeviceModel.chain().find({ udid: device.udid }).data();
-    expect(allocatedSimulator[0].busy).to.be.true;
-    expect(allocatedSimulator[0].name).to.match(/^iPhone/);
-  });
-
-  it('Should find free iPad simulator when app path has .app extension and set busy status to true', async () => {
-    const deviceManager = new DeviceFarmManager({
-      platform: 'ios',
-      includeSimulators: true,
-    });
-    Container.set(DeviceFarmManager, deviceManager);
-    await updateDeviceList();
-    const capabilities = {
-      alwaysMatch: {
-        platformName: 'iOS',
-        'appium:app': '/Downloads/VodQA.app',
-        'appium:iPadOnly': true,
-        'appium:deviceAvailabilityTimeout': 1800,
-        'appium:deviceRetryInterval': 100,
-      },
-      firstMatch: [{}],
-    };
-    const device = await DevicePlugin.allocateDeviceForSession(capabilities);
-    const allocatedSimulator = DeviceModel.chain().find({ udid: device.udid }).data();
-    expect(allocatedSimulator[0].busy).to.be.true;
-    expect(allocatedSimulator[0].name).to.match(/^iPad/);
   });
 });
