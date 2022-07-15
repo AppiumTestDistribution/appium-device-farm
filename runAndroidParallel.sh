3f9376069b87c0a8e0a113fb8f6facae7115a8c5
saikrishna321 <saikrishna321@yahoo.com>
2022-07-15 14:52:26 +0530

    wait for server to start

--- runAndroidParallel.sh
+++ runAndroidParallel.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+until $(curl --output /dev/null --silent --head --fail http://localhost:4723/device-farm/); do
+    printf '.'
+    sleep 5
+done
+
 device=$(curl http://localhost:4723/device-farm/api/devices/android/count)
 echo $device
 ./node_modules/.bin/_mocha --require ts-node/register -p --jobs=$device ./test/e2e/android/*.spec.js --timeout 260000
