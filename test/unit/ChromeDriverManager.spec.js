f4722ed3faee81680b73d48d743801843066d295
saikrishna321 <saikrishna321@yahoo.com>
2022-11-09 17:11:23 +0530

    fix test

--- test/unit/ChromeDriverManager.spec.js
+++ test/unit/ChromeDriverManager.spec.js
@@ -1,6 +1,6 @@
 import ChromeDriverManager from '../../src/device-managers/ChromeDriverManager';
 import { fs } from '@appium/support';
-import { getModuleRoot } from '../../src/chromeUtils';
+import { getModuleRoot, getChromedriverBinaryPath } from '../../src/chromeUtils';
 
 describe('Chrome Driver Manager', function () {
   this.timeout(500000);
@@ -8,7 +8,7 @@ describe('Chrome Driver Manager', function () {
     const chromeDriverManager = await ChromeDriverManager.getInstance();
     const eightyThree = await chromeDriverManager.downloadChromeDriver(83);
     const eightyFour = await chromeDriverManager.downloadChromeDriver(84);
-    (await fs.readdir(getModuleRoot())).length.should.be.eql(2);
+    (await fs.readdir(getChromedriverBinaryPath(getModuleRoot()))).length.should.be.eql(2);
     console.log(eightyThree, eightyFour);
   });
 });
