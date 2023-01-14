0c43b455212894b8b4e3beacb60b0862cd9cbde5
Sai Krishna <saikrishna321@yahoo.com>
2023-01-14 13:24:24 +0530

    add support for lamdaTest (#605)
    * add support for lamdaTest
    
    * Add .circleci/config.yml (#606)
    
    * Add .circleci/config.yml
    
    * Updated config.yml
    
    * remove iOS integration and move to CirecleCI
    
    * Add files via upload
    
    * Update README.md
    
    * bump version

---
+++ test/e2e/android/cloud/lambdaTest.spec.js
@@ -0,0 +1,36 @@
+import { remote } from 'webdriverio';
+
+const APPIUM_HOST = '127.0.0.1';
+const APPIUM_PORT = 31337;
+const WDIO_PARAMS = {
+  connectionRetryCount: 0,
+  hostname: APPIUM_HOST,
+  port: APPIUM_PORT,
+  path: '/wd/hub/',
+  logLevel: 'info',
+};
+const capabilities = {
+  platformName: 'Android',
+  'appium:isRealMobile': true,
+  'appium:appiumVersion': '2.0',
+  'appium:app': 'lt://APP10160531401673528314460178',
+  'appium:build': 'ATDDevs',
+  'appium:name': 'Device-Farm Plugin',
+  'appium:project': 'Appium Device Farm',
+  'lt:options': {
+    w3c: true,
+  },
+};
+let driver;
+describe('Plugin Test', () => {
+  beforeEach(async () => {
+    driver = await remote({ ...WDIO_PARAMS, capabilities });
+  });
+
+  it('Vertical swipe test', async () => {
+    console.log(await driver.capabilities.deviceUDID);
+    await driver.$('~login').click();
+  });
+
+  afterEach(async () => await driver.deleteSession());
+});
