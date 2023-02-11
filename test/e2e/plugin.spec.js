099674e9fc5b0e089db22d420a93c23920980ecc
saikrishna321 <saikrishna321@yahoo.com>
2023-02-11 19:00:06 +0530

    fix e2e test for add and remove

--- test/e2e/plugin.spec.js
+++ test/e2e/plugin.spec.js
@@ -4,6 +4,7 @@ import NodeDevices from '../../src/device-managers/NodeDevices';
 var chai = require('chai'),
   // eslint-disable-next-line no-unused-vars
   should = chai.should();
+import { expect } from 'chai';
 import axios from 'axios';
 
 describe('Basic Plugin Test', () => {
@@ -43,34 +44,14 @@ describe('Basic Plugin Test', () => {
   });
 
   // Needs a fix
-  it.skip('Remove Android devices from node to hub', async () => {
-    const nodeAndroidDevice = [
-      {
-        adbRemoteHost: null,
-        adbPort: 5039,
-        systemPort: 58296,
-        sdk: '13',
-        realDevice: false,
-        name: 'sdk_gphone64_arm64',
-        busy: false,
-        state: 'device',
-        udid: 'emulator-5551',
-        platform: 'android',
-        deviceType: 'emulator',
-        host: 'http://127.2.1.41:4723',
-        totalUtilizationTimeMilliSec: 7023014,
-        sessionStartTime: 0,
-      },
-    ];
+  it('Remove Android devices from node to hub', async () => {
     let nodeDevices = new NodeDevices('http://localhost:31337');
-    await nodeDevices.postDevicesToHub(nodeAndroidDevice, 'add');
     const devices = (await axios.get('http://localhost:31337/device-farm/api/devices')).data;
     devices.find((d) => d.udid === 'emulator-5551').should.to.be.an('object');
-    nodeAndroidDevice[0].udid = 'emulator-5552';
-    await nodeDevices.postDevicesToHub(nodeAndroidDevice, 'add');
+    await nodeDevices.postDevicesToHub({ udid: 'emulator-5551', host: '127.2.1.41' }, 'remove');
     const updatedDeviceList = (await axios.get('http://localhost:31337/device-farm/api/devices'))
       .data;
-    updatedDeviceList.should.have.lengthOf(2);
-    updatedDeviceList.find((d) => d.udid === 'emulator-5552').should.to.be.an('object');
+    let find = updatedDeviceList.find((d) => d.udid === 'emulator-5551');
+    expect(find).to.be.undefined;
   });
 });
