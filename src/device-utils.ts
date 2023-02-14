17ecb94ad48302e3ce75600800f0938ab429bcd5
Dileep B <dileep.srimani@gmail.com>
2023-02-14 18:10:14 +0530

    Temp fix to handle cache file corruption. (#645)
    * Temp fix to handle cache file corruption.
    
    * Handle corrupted Utilization-Time to avoid dashboard breakage.

--- src/device-utils.ts
+++ src/device-utils.ts
@@ -146,10 +146,17 @@ function getStorage() {
  * @returns number
  */
 export async function getUtilizationTime(udid: string) {
-  const value = await getStorage().getItem(udid);
-  if (value !== undefined) {
-    return value;
+  try {
+    const value = await getStorage().getItem(udid);
+    if (value !== undefined && value && !isNaN(value)) {
+      return value;
+    } else {
+      throw `Custom Exception: Utilizaiton time in cache is corrupted. Value = '${value}'.`;
+    }
+  } catch (err) {
+    logger.error(`Failed to fetch Utilization Time \n ${err}`);
   }
+
   return 0;
 }
 
