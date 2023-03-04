3a00b0f9e9892abe02b202e30e6f839ddd4ed5a1
Sai Krishna <saikrishna321@yahoo.com>
2023-03-04 09:06:52 +0530

    Fix go-ios path (#674)
    * improve error message when right app is not provided for ios
    
    * fix go-ios path
    
    * bump version for release

--- test/integration/02iOSDevices.spec.js
+++ test/integration/02iOSDevices.spec.js
@@ -7,10 +7,16 @@ import {
   allocateDeviceForSession,
   initlializeStorage,
 } from '../../src/device-utils';
+import { CLIArgs } from '../../src/data-service/db';
 
 describe('IOS Test', () => {
   it('Throw error when no device is found for given capabilities', async () => {
     await initlializeStorage();
+    CLIArgs.chain()
+      .find()
+      .update(function (d) {
+        d.plugin['device-farm'].iosDeviceType = 'real';
+      });
     const deviceManager = new DeviceFarmManager({
       platform: 'iOS',
       deviceTypes: 'real',
@@ -34,6 +40,35 @@ describe('IOS Test', () => {
         .with.property(
           'message',
           'No device found for filters: {"platform":"ios","name":"iPhone","deviceType":"real","busy":false}'
+        )
+    );
+  });
+
+  it('Should throw error if the IPA does not match with device type real', async () => {
+    await initlializeStorage();
+    const deviceManager = new DeviceFarmManager({
+      platform: 'iOS',
+      deviceTypes: 'real',
+      cliArgs: { plugin: { 'device-farm': { port: 4723, plugin: '' } } },
+    });
+    Container.set(DeviceFarmManager, deviceManager);
+    await updateDeviceList();
+    const capabilities = {
+      alwaysMatch: {
+        platformName: 'iOS',
+        'appium:app': '/Downloads/VodQA.zip',
+        'appium:iPhoneOnly': true,
+        'appium:deviceAvailabilityTimeout': 1800,
+        'appium:deviceRetryInterval': 100,
+      },
+      firstMatch: [{}],
+    };
+    await allocateDeviceForSession(capabilities).catch((error) =>
+      expect(error)
+        .to.be.an('error')
+        .with.property(
+          'message',
+          'iosDeviceType value is set to "real" but app provided is not suitable for real device.'
         )
     );
   });
