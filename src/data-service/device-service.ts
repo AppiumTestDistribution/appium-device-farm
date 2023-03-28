0ee8a5d2dd3d64deb2c46f7e17bf6bf05482320c
Joel Whitney <whitney.joel.b@gmail.com>
2023-03-28 08:18:25 -0400

    Only update device when unblocking if device found that matches filter

--- src/data-service/device-service.ts
+++ src/data-service/device-service.ts
@@ -148,22 +148,22 @@ export async function unblockDevice(filter: object) {
   const device = DeviceModel.chain().find(filter).data()[0];
   if (device !== undefined) {
     console.log(`Found device with udid ${device.udid} to unblock with filter ${JSON.stringify(filter)}`);
+    const sessionStart = device.sessionStartTime;
+    const currentTime = new Date().getTime();
+    const utilization = currentTime - sessionStart;
+    const totalUtilization = device.totalUtilizationTimeMilliSec + utilization;
+    await setUtilizationTime(device.udid, totalUtilization);
+    DeviceModel.chain()
+      .find(filter)
+      .update(function (device: IDevice) {
+        device.session_id = undefined;
+        device.busy = false;
+        device.lastCmdExecutedAt = undefined;
+        device.sessionStartTime = 0;
+        device.totalUtilizationTimeMilliSec = totalUtilization;
+        device.newCommandTimeout = undefined;
+      });
   } else {
     console.log(`Not able to find device to unblock with filter ${JSON.stringify(filter)}`);
   }
-  const sessionStart = device.sessionStartTime;
-  const currentTime = new Date().getTime();
-  const utilization = currentTime - sessionStart;
-  const totalUtilization = device.totalUtilizationTimeMilliSec + utilization;
-  await setUtilizationTime(device.udid, totalUtilization);
-  DeviceModel.chain()
-    .find(filter)
-    .update(function (device: IDevice) {
-      device.session_id = undefined;
-      device.busy = false;
-      device.lastCmdExecutedAt = undefined;
-      device.sessionStartTime = 0;
-      device.totalUtilizationTimeMilliSec = totalUtilization;
-      device.newCommandTimeout = undefined;
-    });
 }
