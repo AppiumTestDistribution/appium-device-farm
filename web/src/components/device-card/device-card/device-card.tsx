26213ee59220269bf59d1cfd320dc3f2eafe7024
Gurleen <61617558+Gurleen-Makkar@users.noreply.github.com>
2023-04-17 22:51:19 +0530

    UI update (#710)

--- web/src/components/device-card/device-card/device-card.tsx
+++ web/src/components/device-card/device-card/device-card.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './device-card.css';
 import { ReactComponent as AndroidIcon } from '../../../assets/android-icon.svg';
-import { ReactComponent as AppleIcon } from '../../../assets/apple-new-icon.svg';
-import { ReactComponent as SessionIcon } from '../../../assets/session-icon.svg';
+import { ReactComponent as AppleIcon } from '../../../assets/apple-icon.svg';
+import { ReactComponent as LinkIcon } from '../../../assets/link-icon.svg';
 import { IDevice } from '../../../interfaces/IDevice';
 import prettyMilliseconds from 'pretty-ms';
 import DeviceFarmApiService from '../../../api-service';
+import CancelGreenIcon from '../../../assets/cancel-green-icon.svg';
+import CancelRedIcon from '../../../assets/cancel-red-icon.svg';
 
 interface IDeviceCardProps {
   device: IDevice;
@@ -78,7 +80,9 @@ export default class DeviceCard extends React.Component<IDeviceCardProps, any> {
       <div className={`device-info-card-container ${this.getStatusClassName()}`}>
         <div className={`device-state ${deviceState}`}>{deviceState}</div>
         <div className="device-info-card-container__title_wrapper">
-          <div className="code device-info-card-container__device-title">{udid}</div>
+          <div className="code device-info-card-container__device-title" title={udid}>
+            {udid}
+          </div>
           {['ios', 'tvos'].includes(platform) ? (
             <AppleIcon className="device-info-card-container__device-icon" />
           ) : (
@@ -121,11 +125,29 @@ export default class DeviceCard extends React.Component<IDeviceCardProps, any> {
               </div>
             </div>
           )}
+          {dashboard_link && !!total_session_count && total_session_count > 0 && (
+            <div className="dashboard-link-wrapper">
+              <div>
+                <div className="device-info-card-container__body_row_label">
+                  {`Session${total_session_count > 1 ? 's' : ''}:`}
+                </div>
+              </div>
+              <div className="dashboard-link">
+                <LinkIcon className="link-icon" />
+                <a className="footer-deeplink" href={dashboard_link} target="_blank">
+                  Appium Dashboard ({total_session_count})
+                </a>
+              </div>
+            </div>
+          )}
+        </div>
+        <div className="device-info-card-container__footer_wrapper">
           {busy && userBlocked && (
             <button
               className="device-info-card__body_unblock-device"
               onClick={() => this.unblockDevice(sdk, platform, udid, deviceState)}
             >
+              <img src={CancelRedIcon} className="device-info-card__body_block-device-icon" />
               Unblock Device
             </button>
           )}
@@ -134,18 +156,15 @@ export default class DeviceCard extends React.Component<IDeviceCardProps, any> {
               className="device-info-card__body_block-device"
               onClick={() => this.blockDevice(sdk, platform, udid, deviceState)}
             >
+              <img src={CancelGreenIcon} className="device-info-card__body_block-device-icon" />
               Block Device
             </button>
           )}
-        </div>
-        <div className="device-info-card-container__footer_wrapper">
-          {dashboard_link && !!total_session_count && total_session_count > 0 && (
-            <div className="dashboard-link-wrapper">
-              <SessionIcon className="footer-icon" />
-              <a className="footer-deeplink" href={dashboard_link} target="_blank">
-                {`${total_session_count} session${total_session_count > 1 ? 's' : ''}`}
-              </a>
-            </div>
+          {busy && !userBlocked && (
+            <button className="device-info-card__body_block-device-disabled">
+              <img src={CancelGreenIcon} className="device-info-card__body_block-device-icon" />
+              Block Device
+            </button>
           )}
         </div>
       </div>
