696a5afab880755178053445ec776bb343636d85
saikrishna321 <saikrishna321@yahoo.com>
2023-04-23 16:20:15 +0800

    look for go-ios globally if local module fails

--- src/device-managers/iOSTracker.ts
+++ src/device-managers/iOSTracker.ts
@@ -1,9 +1,11 @@
 import _ from 'lodash';
 import { EventEmitter } from 'stream';
-import { SubProcess } from 'teen_process';
+import { SubProcess, exec } from 'teen_process';
 import fs from 'fs';
 import os from 'os';
 import path from 'path';
+import { isWindows } from '../helpers';
+import logger from '../logger';
 
 export class GoIosTracker extends EventEmitter {
   private deviceMap: Map<number, string> = new Map();
@@ -14,14 +16,29 @@ export class GoIosTracker extends EventEmitter {
     if (!_.isNil(this.process) && this.process.isRunning) {
       return;
     }
-
-    const files = fs.readdirSync(path.join(__dirname + '../../../node_modules/go-ios/dist/'));
-    const goIOS = files.find((value) => value.includes(os.type().toLowerCase()));
-    this.process = new SubProcess(
-      `${path.join(__dirname + '../../../node_modules/go-ios/dist/')}${goIOS}/ios`,
-      ['listen']
-    );
-
+    try {
+      const files = fs.readdirSync(path.join(__dirname + '../../../node_modules/go-ios/dist/'));
+      const goIOS = files.find((value) => value.includes(os.type().toLowerCase()));
+      fs.lstatSync(
+        `${path.join(__dirname + '../../../node_modules/go-ios/dist/')}${goIOS}/ios`
+      ).isDirectory();
+      this.process = new SubProcess(
+        `${path.join(__dirname + '../../../node_modules/go-ios/dist/')}${goIOS}/ios`,
+        ['listen']
+      );
+    } catch (err) {
+      logger.warn('Had trouble finding go-ios within device-farm modules, checking globally');
+      let { stdout: binPath } = await exec('npm', ['bin', '-g']);
+      if (!binPath || binPath.length == 0) {
+        binPath = path.join(process.env.npm_config_prefix as string, 'bin');
+      }
+      const goIosPath = path.join(binPath.trim(), 'ios' + (isWindows() ? '.exe' : ''));
+      const goIosInstalled = fs.existsSync(goIosPath) && fs.lstatSync(goIosPath).isFile();
+      if (!goIosInstalled)
+        throw new Error('Failed to find go-ios, please install globally npm install -g go-ios');
+      logger.info('Found go-ios globally!');
+      this.process = new SubProcess(goIosPath, ['listen']);
+    }
     this.process.on('lines-stdout', (out) => {
       const parsedOutput = this.parseOutput(out);
       if (!_.isNil(parsedOutput)) {
