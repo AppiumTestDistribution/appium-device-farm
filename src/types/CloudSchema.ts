9b1977d08fdceb93228c68f6a9572af9fac75580
narmadha956 <50320283+narmadha956@users.noreply.github.com>
2023-09-25 12:07:03 +0530

    Added schema validation for server config (#807)
    * Added schema validation for server config
    
    ---------
    
    Co-authored-by: Narmadha Rani Ramasamy <“narmadha956@gmail.com”>

---
+++ src/types/CloudSchema.ts
@@ -0,0 +1,131 @@
+export const browserStackSchema = {
+  $schema: 'http://json-schema.org/draft-07/schema',
+  title: 'browserstackConfig',
+  type: 'object',
+  required: ['cloudName', 'url', 'devices'],
+  properties: {
+    cloudName: {
+      type: 'string',
+      description: 'specify cloudName as browserstack',
+    },
+    url: {
+      type: 'string',
+      description: 'Url of cloud',
+    },
+    devices: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          deviceName: {
+            type: 'string',
+          },
+          os_version: {
+            type: 'string',
+          },
+          platform: {
+            type: 'string',
+          },
+        },
+        required: ['deviceName', 'os_version', 'platform'],
+      },
+    },
+  },
+};
+
+export const sauceOrLambdaSchema = {
+  $schema: 'http://json-schema.org/draft-07/schema',
+  title: 'sauceLabsOrLambdaConfig',
+  type: 'object',
+  required: ['cloudName', 'url', 'devices'],
+  properties: {
+    cloudName: {
+      type: 'string',
+      description: 'specify cloudName as sauce or lambdatest',
+    },
+    url: {
+      type: 'string',
+      description: 'Url of cloud',
+    },
+    devices: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          deviceName: {
+            type: 'string',
+          },
+          platformVersion: {
+            type: 'string',
+          },
+          platform: {
+            type: 'string',
+          },
+        },
+        required: ['deviceName', 'platformVersion', 'platform'],
+      },
+    },
+  },
+};
+
+export const pCloudySchema = {
+  $schema: 'http://json-schema.org/draft-07/schema',
+  title: 'pCloudyConfig',
+  type: 'object',
+  required: ['cloudName', 'url', 'devices'],
+  properties: {
+    cloudName: {
+      type: 'string',
+      description: 'specify cloudName as pCloudy',
+    },
+    url: {
+      type: 'string',
+      description: 'Url of cloud',
+    },
+    devices: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          pCloudy_DeviceManufacturer: {
+            type: 'string',
+          },
+          pCloudy_DeviceVersion: {
+            type: 'string',
+          },
+          platform: {
+            type: 'string',
+          },
+        },
+        required: ['pCloudy_DeviceManufacturer', 'pCloudy_DeviceVersion', 'platform'],
+      },
+    },
+  },
+};
+
+export const defaultSchema = {
+  $schema: 'http://json-schema.org/draft-07/schema',
+  title: 'defaultCloudConfig',
+  type: 'object',
+  required: ['cloudName', 'url', 'devices'],
+  properties: {
+    cloudName: {
+      type: 'string'
+    },
+    url: {
+      type: 'string'
+    },
+    devices: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          platform: {
+            type: 'string',
+          },
+        },
+        required: ['platform'],
+      },
+    },
+  },
+};
