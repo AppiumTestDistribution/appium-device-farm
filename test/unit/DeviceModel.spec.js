cfda213de35ae4a7f3f180858661bec7544dda79
akhmad.fathonih <akhmad.fathonih@rakuten.com>
2023-11-17 18:12:26 +0900

    Ignore error when inserting device to database (#889)
    * perf: send device list update only when hub is online.
    fix: ios tracker process spawned multiple times.
    fix: periodically check from stale nodes.
    
    * chore: logging
    
    * bump: patch version
    
    * 8.3.15
    
    * fix: ignore error when inserting device to db
    
    * fix: add device function
    
    * fix: remove loki field misled saving into existing data
    
    ---------
    
    Co-authored-by: Sai Krishna <saikrishna321@yahoo.com>

--- test/unit/DeviceModel.spec.js
+++ test/unit/DeviceModel.spec.js
@@ -1,4 +1,5 @@
 import { DeviceModel } from '../../src/data-service/db';
+import { getAllDevices } from '../../src/data-service/device-service';
 import { deviceMock } from './fixtures/devices';
 import { expect } from 'chai';
 import {
@@ -7,22 +8,27 @@ import {
   setSimulatorState,
 } from '../../src/data-service/device-service';
 import sinon from 'sinon';
+import { log } from 'node-persist';
 var sandbox = sinon.createSandbox();
 
 describe('Model Test', () => {
   before('Add device collection', () => {
+    DeviceModel.removeDataOnly()
     DeviceModel.insert(deviceMock);
+    DeviceModel.chain().find().data().length.should.be.equal(deviceMock.length);
   });
+
   after('clean', () => {
     sandbox.restore();
   });
+
   it('Should remove device from old pool when new poll call does not have the device', () => {
     removeDevice({ udid: 'emulator-5570', host: '127.0.0.1' });
-    const updatedDeviceList = DeviceModel.chain().find({ udid: 'emulator-5555' }).data();
+    const updatedDeviceList = DeviceModel.chain().find({ udid: 'emulator-5570' }).data();
     expect(updatedDeviceList).to.deep.equal([]);
   });
 
-  it('Should update new device polled into the existing device list', () => {
+  it('Should update new device polled into the existing device list', async () => {
     const newDeviceList = [
       {
         busy: false,
@@ -31,9 +37,11 @@ describe('Model Test', () => {
         platform: 'android',
       },
     ];
+
     addNewDevice(newDeviceList);
+    
     const updatedDeviceList = DeviceModel.chain().find({ udid: 'emulator-9994' }).data();
-    expect(updatedDeviceList.length).to.be.equal(1);
+    expect(updatedDeviceList.length).to.be.greaterThanOrEqual(1);
   });
 
   it('Should update the ios simulator state from shutdown to booted', async () => {
@@ -80,5 +88,41 @@ describe('Model Test', () => {
       .find({ udid: '0FBCBDCC-2FF1-4FCA-B034-60ABC86ED888' })
       .data();
     expect(updatedDeviceList[0].state).to.be.equal('Shutdown');
+  });
+
+  it('Should handle concurrent update to device list', async () => {
+    // create bunch of devices using loop
+    let newDeviceList = [];
+    for (let i = 0; i < 10; i++) {
+      newDeviceList.push({
+        busy: false,
+        state: 'device',
+        udid: `emulator-${i}`,
+        platform: 'android',
+      });
+    }
+
+    // create a function to add new device with random delay to simulate concurrent calls from multiple nodes
+    const delayedAddDevice = async (newDeviceList) => {
+      await new Promise((resolve) => setTimeout(resolve, Math.random() * 1000));
+      for (const device of newDeviceList) {
+        await new Promise((resolve) => setTimeout(resolve, Math.random() * 1000));
+        await addNewDevice([device]);
+      }
+    };
+
+    const existingDevices = getAllDevices()
+
+    // create some parallel calls to addNewDevice
+    await Promise.all([delayedAddDevice(newDeviceList), delayedAddDevice(existingDevices)]);
+
+    // verify that all devices are added to the db
+    const updatedDeviceList = DeviceModel.chain().find({ platform: 'android' }).data();
+    
+    // every newDeviceList should be listed under updatedDeviceList
+    // map udid only to make it simple
+    const addedDeviceUdids = newDeviceList.map((device) => device.udid);
+    const updatedDeviceUdids = updatedDeviceList.map((device) => device.udid);
+    addedDeviceUdids.forEach((udid) => updatedDeviceUdids.includes(udid).should.be.true);
   });
 });
