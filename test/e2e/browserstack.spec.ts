984249935bde4821d73a27587965d9d88e6398e3
Akhmad Fathonih <akhmad.fathonih@rakuten.com>
2023-12-01 10:38:21 +0900

    ci: remove duplicate script ci: fix browserstack spec

--- test/e2e/browserstack.spec.ts
+++ test/e2e/browserstack.spec.ts
@@ -1,10 +1,40 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+import { pluginE2EHarness } from '@appium/plugin-test-support';
 import axios from 'axios';
 import { expect } from 'chai';
+import { ensureHubConfig, ensureNodeConfig, ensureAppiumHome, HUB_APPIUM_PORT, PLUGIN_PATH } from './e2ehelper';
+import ip from 'ip';
+import path from 'path';
 
 describe('Browserstack Devices', () => {
+  // dump hub config into a file
+  const hub_config_file = path.join(__dirname, '../../serverConfig/bs-config.json');
+
+  // setup appium home
+  const APPIUM_HOME = ensureAppiumHome();
+
+  // run hub
+  pluginE2EHarness({
+    before: global.before,
+    after: global.after,
+    serverArgs: {
+      subcommand: 'server',
+      configFile: hub_config_file
+    },
+    pluginName: 'device-farm',
+    port: HUB_APPIUM_PORT,
+    driverSource: 'npm',
+    driverName: 'uiautomator2',
+    driverSpec: 'appium-uiautomator2-driver',
+    pluginSource: 'local',
+    pluginSpec: PLUGIN_PATH,
+    appiumHome: APPIUM_HOME!
+  })
+
+  const hub_url = `http://${ip.address()}:${HUB_APPIUM_PORT}`;
+  
   it('Should be able to run the android with Browerstack config', async () => {
-    let androidDevices = (await axios.get('http://localhost:31337/device-farm/api/devices/android'))
+    let androidDevices = (await axios.get(`${hub_url}/device-farm/api/devices/android`))
       .data;
     delete androidDevices[0].meta;
     delete androidDevices[0]['$loki'];
@@ -26,12 +56,12 @@ describe('Browserstack Devices', () => {
   });
 
   it('Should be able to run the plugin with Browerstack config', async () => {
-    const status = (await axios.get('http://localhost:31337/device-farm/api/devices')).status;
+    const status = (await axios.get(`${hub_url}/device-farm/api/devices`)).status;
     expect(status).to.be.eql(200);
   });
 
   it('Should be able to get iOS devices from Browerstack config', async () => {
-    let iosDevics = (await axios.get('http://localhost:31337/device-farm/api/devices/ios')).data;
+    let iosDevics = (await axios.get(`${hub_url}/device-farm/api/devices/ios`)).data;
     delete iosDevics[0].meta;
     delete iosDevics[0]['$loki'];
     expect(iosDevics[0]).to.deep.equal({
