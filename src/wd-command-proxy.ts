6175577615c88fa43ec35783dd8aa07da910b92b
Akhmad Fathonih <akhmad.fathonih@rakuten.com>
2023-12-05 09:40:03 +0900

    fix: don't use origin for proxy-middleware

--- src/wd-command-proxy.ts
+++ src/wd-command-proxy.ts
@@ -4,8 +4,8 @@ import { HttpProxyAgent } from 'http-proxy-agent';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 import _ from 'lodash';
 import { unblockDevice } from './data-service/device-service';
-import logger from './logger';
 import axios from 'axios';
+import log from './logger';
 
 const remoteProxyMap: Map<string, any> = new Map();
 const remoteHostMap: Map<string, any> = new Map();
@@ -16,17 +16,19 @@ function getProxyServer() {
 
 export function addProxyHandler(sessionId: string, remoteHost: string) {
   const proxyServer = getProxyServer();
-  logger.info(`proxyServer: ${JSON.stringify(proxyServer)}`);
   const config: any = {
-    target: new URL(remoteHost).origin,
+    target: new URL(remoteHost),
     logLevel: 'debug',
     changeOrigin: true,
     onProxyReq: proxyRequestInterceptor,
   };
+
   if (proxyServer) {
-    logger.info(`Added proxy to createProxyMiddleware: ${JSON.stringify(proxyServer)}`);
+    log.info(`Added proxy to createProxyMiddleware: ${JSON.stringify(proxyServer)}`);
     config.agent = new HttpsProxyAgent(proxyServer);
   }
+
+  log.info(`Creating remote proxy for session ${sessionId} to ${remoteHost}`);
   remoteProxyMap.set(sessionId, createProxyMiddleware(config));
   remoteHostMap.set(sessionId, remoteHost);
 }
@@ -84,7 +86,7 @@ async function handler(req: Request, res: Response, next: NextFunction) {
       remoteProxyMap.get(sessionId)(req, res, next);
     }
     if (req.method === 'DELETE') {
-      logger.info(
+      log.info(
         `📱 Unblocking the device that is blocked for session ${sessionId} in remote machine`,
       );
       unblockDevice({ session_id: sessionId });
@@ -96,7 +98,7 @@ async function handler(req: Request, res: Response, next: NextFunction) {
 }
 
 export function registerProxyMiddlware(expressApp: any) {
-  logger.info('Registering proxy middleware');
+  log.info('Registering proxy middleware');
   const index = expressApp._router.stack.findIndex((s: any) => s.route);
   expressApp.use('/', handler);
   expressApp._router.stack.splice(index, 0, expressApp._router.stack.pop());
