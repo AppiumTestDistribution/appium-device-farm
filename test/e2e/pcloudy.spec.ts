f40cca81baf3f376f6947122a30d33917e19e831
Akhmad Fathonih <akhmad.fathonih@rakuten.com>
2024-01-16 09:47:19 +0900

    test: fixes

--- test/e2e/pcloudy.spec.ts
+++ test/e2e/pcloudy.spec.ts
@@ -92,8 +92,6 @@ describe('PCloudy Devices', () => {
       sdk: '15.1',
       udid: 'APPLE',
       offline: false,
-      sessionStartTime: 0,
-      totalUtilizationTimeMilliSec: null,
       name: 'APPLE',
     });
   });
