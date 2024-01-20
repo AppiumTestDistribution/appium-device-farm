6ccb904afcb68547057f835b245bf86c7397b730
saikrishna321 <saikrishna321@yahoo.com>
2024-01-20 21:56:48 +0800

    add unique udid for cloud devices

--- src/device-managers/cloud/Devices.ts
+++ src/device-managers/cloud/Devices.ts
@@ -10,6 +10,7 @@ import {
 import logger from '../../logger';
 import { Schema, Validator } from 'jsonschema';
 import { CloudConfig, CloudDevice } from '../../interfaces/IPluginArgs';
+import { v4 as uuidv4 } from 'uuid';
 
 export default class Devices {
   private devices: CloudDevice[];
@@ -33,7 +34,6 @@ export default class Devices {
         cloudDeviceProperties = {
           name: d.deviceName,
           sdk: d['os_version'],
-          udid: d.deviceName,
         };
       }
       if (this.isSauceLabs() || this.isLambdaTest() || this.isHeadSpin()) {
@@ -41,7 +41,6 @@ export default class Devices {
         cloudDeviceProperties = {
           name: d.deviceName,
           sdk: d.platformVersion,
-          udid: d.deviceName,
         };
       }
       if (this.isPCloudy()) {
@@ -49,12 +48,12 @@ export default class Devices {
         cloudDeviceProperties = {
           name: d?.pCloudy_DeviceFullName || d?.pCloudy_DeviceManufacturer,
           sdk: d?.pCloudy_DeviceVersion || d?.platformVersion,
-          udid: d?.pCloudy_DeviceFullName || d?.pCloudy_DeviceManufacturer,
         };
       }
       return Object.assign({}, ...devicesByPlatform, {
         host: this.cloud.url,
         busy: false,
+        udid: uuidv4(),
         userBlocked: false,
         deviceType: 'real',
         capability: d,
