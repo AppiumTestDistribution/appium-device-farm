d4b13df18b4d439b46f509dc2b52a1f430c9fc54
Srinivasan Sekar <srinivasan.sekar1990@gmail.com>
2024-02-13 19:46:04 +0800

    fix api calls to builds and sessions

--- web/src/components/session/session-logs/session-logs.tsx
+++ web/src/components/session/session-logs/session-logs.tsx
@@ -11,12 +11,11 @@ enum ActiveTab {
   AppProfiling = 'appProfiling',
 }
 
-function SessionLogs(sessionId: any) {
+function SessionLogs({ sessionId }: any) {
   const [sessionLogs, setSessionLogs] = useState<ISessionLogs[]>([]);
   const [showImages, setShowImages] = useState(false);
   const [showErrorsOnly, setShowErrorsOnly] = useState(false);
   const [activeTab, setActiveTab] = useState<ActiveTab>(ActiveTab.TextLogs);
-
 
   useEffect(() => {
     async function init() {
@@ -27,80 +26,8 @@ function SessionLogs(sessionId: any) {
         console.log(error);
       }
     }
-    // fetch sessions
-    // const builds: IBuild[] = [
-    //   {
-    //     id: '3ca00106-507f-45ed-8d8f-d9c89f74c7be',
-    //     name: 'Webdriver io weekly build 2023-12-26',
-    //     createdAt: '2023-12-26T16:13:27.287Z',
-    //     updatedAt: '2023-12-26T16:13:27.287Z',
-    //   },
-    //   {
-    //     id: '3ca00106-507f-45ed-8d8f-d89f74c7be',
-    //     name: 'Webdriver io weekly build 2023-12-26',
-    //     createdAt: '2023-12-26T16:13:27.287Z',
-    //     updatedAt: '2023-12-26T16:13:27.287Z',
-    //   },
-    //   {
-    //     id: '3ca00106-507f-45ed-8d8f-d9c894c7be',
-    //     name: 'Webdriver io weekly build 2023-12-26',
-    //     createdAt: '2023-12-26T16:13:27.287Z',
-    //     updatedAt: '2023-12-26T16:13:27.287Z',
-    //   },
-    // ];
-    //
-    // const sessions: ISession[] = [
-    //   {
-    //     id: 'c5fbf9dc-1d84-439b-9bff-dc270bf05a87',
-    //     build_id: '3ca00106-507f-45ed-8d8f-d9c89f74c7be',
-    //     name: 'Test session',
-    //     status: 'running',
-    //     desired_capabilities:
-    //       '{"platformName":"Android","df:build":"Failed webdriver IO build","df:record_video":false,"df:screeenshot_on_failure":true,"deviceName":"Pixel_4_31","platformVersion":"12","orientation":"PORTRAIT","automationName":"UiAutomator2","app":"/Users/sselvarj/Documents/git/oss/appium-boilerplate/apps/Android-NativeDemoApp-0.4.0.apk","appWaitActivity":"com.wdiodemoapp.MainActivity","newCommandTimeout":240,"udid":"emulator-5554","systemPort":57577,"chromeDriverPort":57578,"adbPort":5037,"mjpegServerPort":57575}',
-    //     session_capabilities:
-    //       '{"platformName":"Android","df:build":"Failed webdriver IO build","df:record_video":false,"df:screeenshot_on_failure":true,"deviceName":"emulator-5554","platformVersion":"12","orientation":"PORTRAIT","automationName":"UiAutomator2","app":"/Users/sselvarj/Documents/git/oss/appium-boilerplate/apps/Android-NativeDemoApp-0.4.0.apk","appWaitActivity":"com.wdiodemoapp.MainActivity","newCommandTimeout":240,"udid":"emulator-5554","systemPort":57577,"chromeDriverPort":57578,"adbPort":5037,"mjpegServerPort":57575,"platform":"LINUX","webStorageEnabled":false,"takesScreenshot":true,"javascriptEnabled":true,"databaseEnabled":false,"networkConnectionEnabled":true,"locationContextEnabled":false,"warnings":{},"deviceUDID":"emulator-5554","appPackage":"com.wdiodemoapp","pixelRatio":"2.75","statBarHeight":66,"viewportRect":{"left":0,"top":66,"width":1080,"height":2082},"deviceApiLevel":31,"deviceManufacturer":"Google","deviceModel":"sdk_gphone64_arm64","deviceScreenSize":"1080x2280","deviceScreenDensity":440}',
-    //     node_id: '53146bd9-3d62-4ab7-b1e9-d151e3f1b0ed',
-    //     has_live_video: true, //sample live url - http://127.0.0.1:31337/device-farm/api/session/c5fbf9dc-1d84-439b-9bff-dc270bf05a87/live_video
-    //     video_recording:
-    //       'c5fbf9dc-1d84-439b-9bff-dc270bf05a87/video/c5fbf9dc-1d84-439b-9bff-dc270bf05a87.mp4',
-    //     startTime: '2024-01-13T08:56:20.723Z',
-    //     endTime: null,
-    //     failure_reason: null,
-    //     device_udid: 'emulator-5554',
-    //     device_platform: 'android',
-    //     device_version: '12',
-    //     device_name: 'sdk_gphone64_arm64',
-    //     createdAt: '2023-12-26T16:13:43.723Z',
-    //     updatedAt: '2023-12-26T16:13:43.723Z',
-    //   },
-    //   {
-    //     id: '8544d114-b90f-496e-9929-205eb069e9cd',
-    //     build_id: '3ca00106-507f-45ed-8d8f-d9c89f74c7be',
-    //     name: null,
-    //     status: 'unmarked',
-    //     desired_capabilities:
-    //       '{"platformName":"Android","df:build":"Failed webdriver IO build","df:record_video":false,"df:screeenshot_on_failure":true,"deviceName":"Pixel_4_31","platformVersion":"12","orientation":"PORTRAIT","automationName":"UiAutomator2","app":"/Users/sselvarj/Documents/git/oss/appium-boilerplate/apps/Android-NativeDemoApp-0.4.0.apk","appWaitActivity":"com.wdiodemoapp.MainActivity","newCommandTimeout":240,"udid":"emulator-5554","systemPort":57135,"chromeDriverPort":57136,"adbPort":5037,"mjpegServerPort":57132}',
-    //     session_capabilities:
-    //       '{"platformName":"Android","df:build":"Failed webdriver IO build","df:record_video":false,"df:screeenshot_on_failure":true,"deviceName":"emulator-5554","platformVersion":"12","orientation":"PORTRAIT","automationName":"UiAutomator2","app":"/Users/sselvarj/Documents/git/oss/appium-boilerplate/apps/Android-NativeDemoApp-0.4.0.apk","appWaitActivity":"com.wdiodemoapp.MainActivity","newCommandTimeout":240,"udid":"emulator-5554","systemPort":57135,"chromeDriverPort":57136,"adbPort":5037,"mjpegServerPort":57132,"platform":"LINUX","webStorageEnabled":false,"takesScreenshot":true,"javascriptEnabled":true,"databaseEnabled":false,"networkConnectionEnabled":true,"locationContextEnabled":false,"warnings":{},"deviceUDID":"emulator-5554","appPackage":"com.wdiodemoapp","pixelRatio":"2.75","statBarHeight":66,"viewportRect":{"left":0,"top":66,"width":1080,"height":2082},"deviceApiLevel":31,"deviceManufacturer":"Google","deviceModel":"sdk_gphone64_arm64","deviceScreenSize":"1080x2280","deviceScreenDensity":440}',
-    //     node_id: '53146bd9-3d62-4ab7-b1e9-d151e3f1b0ed',
-    //     has_live_video: false,
-    //     video_recording: null,
-    //     startTime: '2023-12-26T16:13:27.293Z',
-    //     endTime: '2023-12-26T16:13:36.884Z',
-    //     failure_reason: null,
-    //     device_udid: 'emulator-5554',
-    //     device_platform: 'android',
-    //     device_version: '12',
-    //     device_name: 'sdk_gphone64_arm64',
-    //     createdAt: '2023-12-26T16:13:27.293Z',
-    //     updatedAt: '2023-12-26T16:13:36.886Z',
-    //     live: 'http://127.0.0.1:31337/device-farm/api/session/8544d114-b90f-496e-9929-205eb069e9cd/live_video',
-    //   },
-    // ];
-
     init();
   }, []);
-
 
   const handleTabClick = (tab: ActiveTab) => {
     setActiveTab(tab);
@@ -134,7 +61,7 @@ function SessionLogs(sessionId: any) {
           App Profiling
         </div>
       </div>
-      <div className='tab-filter'>
+      <div className="tab-filter">
         <div className="checkbox-container">
           <input
             type="checkbox"
--- web/src/pages/Session/index.tsx
+++ web/src/pages/Session/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import './session.css';
-import RightArrowIcon from '../../assets/right-arrow-icon.svg'
+import RightArrowIcon from '../../assets/right-arrow-icon.svg';
 import { IBuild } from '../../interfaces/IBuild';
 import { ISession } from '../../interfaces/ISession';
 import BuildContainer from '../../components/build-container/build-container';
@@ -19,119 +19,59 @@ function Session() {
   const [sessions, setSessions] = useState<ISession[]>([]);
 
   useEffect(() => {
-    // fetch builds
-    // fetch sessions
     async function init() {
       try {
-        const [builds, sessions] = await Promise.all([DeviceFarmApiService.getBuilds(),
-          DeviceFarmApiService.getSessions()]);
+        const builds = await DeviceFarmApiService.getBuilds();
+        const sessions = await DeviceFarmApiService.getSessions();
         setBuilds(builds);
         setSessions(sessions);
-
+        if (
+          builds.findIndex((build: any) => build.id === buildId) < 0 ||
+          sessions.findIndex((session: any) => session.id === sessionId) < 0
+        ) {
+          navigate('/device-farm/builds');
+          return;
+        }
+        setSelectedBuild(builds.find((build: any) => build.id === buildId));
+        setSelectedSession(sessions.find((session: any) => session.id === sessionId));
+        console.log('*****', JSON.stringify(builds, null, 2));
+        console.log('*****', JSON.stringify(sessions, null, 2));
       } catch (error) {
         console.log(error);
       }
     }
-    // fetch sessions
-    // const builds: IBuild[] = [
-    //   {
-    //     id: '3ca00106-507f-45ed-8d8f-d9c89f74c7be',
-    //     name: 'Webdriver io weekly build 2023-12-26',
-    //     createdAt: '2023-12-26T16:13:27.287Z',
-    //     updatedAt: '2023-12-26T16:13:27.287Z',
-    //   },
-    //   {
-    //     id: '3ca00106-507f-45ed-8d8f-d89f74c7be',
-    //     name: 'Webdriver io weekly build 2023-12-26',
-    //     createdAt: '2023-12-26T16:13:27.287Z',
-    //     updatedAt: '2023-12-26T16:13:27.287Z',
-    //   },
-    //   {
-    //     id: '3ca00106-507f-45ed-8d8f-d9c894c7be',
-    //     name: 'Webdriver io weekly build 2023-12-26',
-    //     createdAt: '2023-12-26T16:13:27.287Z',
-    //     updatedAt: '2023-12-26T16:13:27.287Z',
-    //   },
-    // ];
-    //
-    // const sessions: ISession[] = [
-    //   {
-    //     id: 'c5fbf9dc-1d84-439b-9bff-dc270bf05a87',
-    //     build_id: '3ca00106-507f-45ed-8d8f-d9c89f74c7be',
-    //     name: 'Test session',
-    //     status: 'running',
-    //     desired_capabilities:
-    //       '{"platformName":"Android","df:build":"Failed webdriver IO build","df:record_video":false,"df:screeenshot_on_failure":true,"deviceName":"Pixel_4_31","platformVersion":"12","orientation":"PORTRAIT","automationName":"UiAutomator2","app":"/Users/sselvarj/Documents/git/oss/appium-boilerplate/apps/Android-NativeDemoApp-0.4.0.apk","appWaitActivity":"com.wdiodemoapp.MainActivity","newCommandTimeout":240,"udid":"emulator-5554","systemPort":57577,"chromeDriverPort":57578,"adbPort":5037,"mjpegServerPort":57575}',
-    //     session_capabilities:
-    //       '{"platformName":"Android","df:build":"Failed webdriver IO build","df:record_video":false,"df:screeenshot_on_failure":true,"deviceName":"emulator-5554","platformVersion":"12","orientation":"PORTRAIT","automationName":"UiAutomator2","app":"/Users/sselvarj/Documents/git/oss/appium-boilerplate/apps/Android-NativeDemoApp-0.4.0.apk","appWaitActivity":"com.wdiodemoapp.MainActivity","newCommandTimeout":240,"udid":"emulator-5554","systemPort":57577,"chromeDriverPort":57578,"adbPort":5037,"mjpegServerPort":57575,"platform":"LINUX","webStorageEnabled":false,"takesScreenshot":true,"javascriptEnabled":true,"databaseEnabled":false,"networkConnectionEnabled":true,"locationContextEnabled":false,"warnings":{},"deviceUDID":"emulator-5554","appPackage":"com.wdiodemoapp","pixelRatio":"2.75","statBarHeight":66,"viewportRect":{"left":0,"top":66,"width":1080,"height":2082},"deviceApiLevel":31,"deviceManufacturer":"Google","deviceModel":"sdk_gphone64_arm64","deviceScreenSize":"1080x2280","deviceScreenDensity":440}',
-    //     node_id: '53146bd9-3d62-4ab7-b1e9-d151e3f1b0ed',
-    //     has_live_video: true, //sample live url - http://127.0.0.1:31337/device-farm/api/session/c5fbf9dc-1d84-439b-9bff-dc270bf05a87/live_video
-    //     video_recording:
-    //       'c5fbf9dc-1d84-439b-9bff-dc270bf05a87/video/c5fbf9dc-1d84-439b-9bff-dc270bf05a87.mp4',
-    //     startTime: '2024-01-13T08:56:20.723Z',
-    //     endTime: null,
-    //     failure_reason: null,
-    //     device_udid: 'emulator-5554',
-    //     device_platform: 'android',
-    //     device_version: '12',
-    //     device_name: 'sdk_gphone64_arm64',
-    //     createdAt: '2023-12-26T16:13:43.723Z',
-    //     updatedAt: '2023-12-26T16:13:43.723Z',
-    //   },
-    //   {
-    //     id: '8544d114-b90f-496e-9929-205eb069e9cd',
-    //     build_id: '3ca00106-507f-45ed-8d8f-d9c89f74c7be',
-    //     name: null,
-    //     status: 'unmarked',
-    //     desired_capabilities:
-    //       '{"platformName":"Android","df:build":"Failed webdriver IO build","df:record_video":false,"df:screeenshot_on_failure":true,"deviceName":"Pixel_4_31","platformVersion":"12","orientation":"PORTRAIT","automationName":"UiAutomator2","app":"/Users/sselvarj/Documents/git/oss/appium-boilerplate/apps/Android-NativeDemoApp-0.4.0.apk","appWaitActivity":"com.wdiodemoapp.MainActivity","newCommandTimeout":240,"udid":"emulator-5554","systemPort":57135,"chromeDriverPort":57136,"adbPort":5037,"mjpegServerPort":57132}',
-    //     session_capabilities:
-    //       '{"platformName":"Android","df:build":"Failed webdriver IO build","df:record_video":false,"df:screeenshot_on_failure":true,"deviceName":"emulator-5554","platformVersion":"12","orientation":"PORTRAIT","automationName":"UiAutomator2","app":"/Users/sselvarj/Documents/git/oss/appium-boilerplate/apps/Android-NativeDemoApp-0.4.0.apk","appWaitActivity":"com.wdiodemoapp.MainActivity","newCommandTimeout":240,"udid":"emulator-5554","systemPort":57135,"chromeDriverPort":57136,"adbPort":5037,"mjpegServerPort":57132,"platform":"LINUX","webStorageEnabled":false,"takesScreenshot":true,"javascriptEnabled":true,"databaseEnabled":false,"networkConnectionEnabled":true,"locationContextEnabled":false,"warnings":{},"deviceUDID":"emulator-5554","appPackage":"com.wdiodemoapp","pixelRatio":"2.75","statBarHeight":66,"viewportRect":{"left":0,"top":66,"width":1080,"height":2082},"deviceApiLevel":31,"deviceManufacturer":"Google","deviceModel":"sdk_gphone64_arm64","deviceScreenSize":"1080x2280","deviceScreenDensity":440}',
-    //     node_id: '53146bd9-3d62-4ab7-b1e9-d151e3f1b0ed',
-    //     has_live_video: false,
-    //     video_recording: null,
-    //     startTime: '2023-12-26T16:13:27.293Z',
-    //     endTime: '2023-12-26T16:13:36.884Z',
-    //     failure_reason: null,
-    //     device_udid: 'emulator-5554',
-    //     device_platform: 'android',
-    //     device_version: '12',
-    //     device_name: 'sdk_gphone64_arm64',
-    //     createdAt: '2023-12-26T16:13:27.293Z',
-    //     updatedAt: '2023-12-26T16:13:36.886Z',
-    //     live: 'http://127.0.0.1:31337/device-farm/api/session/8544d114-b90f-496e-9929-205eb069e9cd/live_video',
-    //   },
-    // ];
-
     init();
-    if (builds.findIndex(build => build.id === buildId) < 0 || sessions.findIndex(session => session.id === sessionId) < 0) {
-      navigate('/device-farm/builds');
-      return;
-    }
-    setSelectedBuild(builds.find(build => build.id === buildId));
-    setSelectedSession(sessions.find(session => session.id === sessionId));
   }, [buildId, navigate, sessionId]);
 
   const handleSelectedBuildChange = (buildId: string) => {
     navigate(`/device-farm/builds?buildId=${buildId}`);
-  }
+  };
 
   return (
     <div className="app-container">
       <div className="app-body-container">
-        <BuildContainer selectedBuild={selectedBuild} handleBuildClick={handleSelectedBuildChange} builds={builds} sessions={sessions} />
-        {selectedSession && <div className="session-container">
-          <div className='session-header'>
-            <button onClick={() => handleSelectedBuildChange(selectedSession.build_id)}>Build</button>
-            <img src={RightArrowIcon} alt='right-arrow' />
-            <h3>{selectedSession.name || "Not Available"}</h3>
+        <BuildContainer
+          selectedBuild={selectedBuild}
+          handleBuildClick={handleSelectedBuildChange}
+          builds={builds}
+          sessions={sessions}
+        />
+        {selectedSession && (
+          <div className="session-container">
+            <div className="session-header">
+              <button onClick={() => handleSelectedBuildChange(selectedSession.build_id)}>
+                Build
+              </button>
+              <img src={RightArrowIcon} alt="right-arrow" />
+              <h3>{selectedSession.name || 'Not Available'}</h3>
+            </div>
+            <SessionInfo session={selectedSession} />
+            <div className="session-body">
+              <Capabilities session={selectedSession} />
+              <SessionLogs sessionId={selectedSession.id} />
+            </div>
           </div>
-          <SessionInfo session={selectedSession} />
-          <div className='session-body'>
-            <Capabilities session={selectedSession} />
-            <SessionLogs sessionId={sessionId}/>
-          </div>
-        </div>}
+        )}
       </div>
     </div>
   );
