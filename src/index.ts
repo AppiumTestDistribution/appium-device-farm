f667b4ba61724b51686358240142d9cc9db96022
Sudharsan Selvaraj <sudharsanselvaraj.c@gmail.com>
2024-03-07 17:33:57 +0530

    Add listeners for unhandled errors and promise rejection (#1048)
    * Add listeners for unhandled errors and promise rejection
    
    * fix logging

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { DevicePlugin } from './plugin';
 import { path as ffmpeg } from '@ffmpeg-installer/ffmpeg';
-import { loadExternalModules } from './helpers';
+import { loadExternalModules, registerErrorHandlers } from './helpers';
 
 //Add FFMPEG to path for appium to record video of the session
 process.env.PATH = process.env.PATH + ':' + ffmpeg.replace(/ffmpeg$/g, '');
 loadExternalModules();
+
+registerErrorHandlers();
 
 export default DevicePlugin;
 export { DevicePlugin };
