7e154775bb83ffda2a85ae0b7eb3b8b760e6c307
saikrishna321 <saikrishna321@yahoo.com>
2024-03-15 18:37:49 +0800

    added websocket listener

--- src/proxy/ws-proxy.ts
+++ src/proxy/ws-proxy.ts
@@ -1,23 +1,23 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
+import ip from 'ip';
 
 const wsServer = new WebSocketServer({
+  host: ip.address(),
   port: 8080,
 });
 
 export function adbStreamingWebSocket(ADB_STREAMING_PORT: number) {
   wsServer.on('connection', (feSocket) => {
-    const adbStreamingOriginalSocket = new WebSocket(`ws://127.0.0.1:${ADB_STREAMING_PORT}`);
+    const adbStreamingOriginalSocket = new WebSocket(`ws://localhost:${ADB_STREAMING_PORT}`);
 
     adbStreamingOriginalSocket.onopen = () => {
       feSocket.on('message', (message) => {
-        console.log(message);
         adbStreamingOriginalSocket.send(message.toString());
       });
     };
 
     adbStreamingOriginalSocket.onmessage = (event) => {
-      console.log(event);
-      feSocket.send(event.data.toString());
+      feSocket.send(event.data);
     };
   });
 }
