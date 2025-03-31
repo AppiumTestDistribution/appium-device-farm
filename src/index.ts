19b0cd1b40fb61f2550dbe00e9f9d25d4c41ed50
Sudharsan Selvaraj <sudharsanselvaraj.c@gmail.com>
2025-03-31 20:03:46 +0530

    fix: address open issues and update base plugin versions (#1631)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { DevicePlugin } from './plugin';
 import ffmpeg from '@ffmpeg-installer/ffmpeg';
 import { loadExternalModules, registerErrorHandlers } from './helpers';
+import { setDefaultResultOrder } from 'dns';
+setDefaultResultOrder('ipv4first');
 
 //Add FFMPEG to path for appium to record video of the session
 process.env.PATH = process.env.PATH + ':' + ffmpeg.path.replace(/ffmpeg$/g, '');
